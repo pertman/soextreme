6b6251f74cb55512e7199a4a2e6dfa823e02a7b1
korentyn <frantzcorentin@gmail.com>
2019-07-05 17:02:33 +0200

    Add activity controller

---
+++ application/controllers/ActivityController.php
@@ -0,0 +1,18 @@
+<?php
+
+
+class ActivityController extends MY_Controller{
+
+    public function __construct()
+    {
+        parent::__construct();
+        parent::init();
+
+    }
+
+    public function showActivityForm(){
+        $this->load->view('activityForm.php');
+    }
+
+
+}

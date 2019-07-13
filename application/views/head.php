50e952b7428587ab152f292dc444be3195efa298
ph2m_rpe <raphael.pertusa@ph2m.com>
2019-07-13 14:18:07 +0200

    npm install + use use bulma from npm

--- application/views/head.php
+++ application/views/head.php
@@ -12,7 +12,6 @@
         integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
         crossorigin="anonymous"></script>
 
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.5/css/bulma.min.css">
-
+    <link rel="stylesheet" type="text/css" href="../../css/style.css">
     <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
 </head>

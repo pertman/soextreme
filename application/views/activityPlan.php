45d06f2f33f699f793c2391dd42ce4f9a108234c
Corentin FRANTZ <frantzcorentin@gmail.com>
2019-07-24 10:28:35 +0200

    ajout contrôle complétion champs date et heure

--- application/views/activityPlan.php
+++ application/views/activityPlan.php
@@ -9,43 +9,43 @@
         <div class="field">
             <label for="act_title">Titre</label>
             <div class="control">
-                <input class="input" value="<?php echo $activity[0]["act_name"]; ?>" type="text" name="act_name" readonly>
+                <input class="input" value="<?php echo $activity["act_name"]; ?>" type="text" name="act_name" readonly>
             </div>
         </div>
         <div class="field">
             <label for="act_description">Description</label>
             <div class="control">
-                <textarea class="textarea"  name="act_description" placeholder="Description de l'activité..." readonly><?php echo $activity[0]["act_description"]; ?></textarea>
+                <textarea class="textarea"  name="act_description" placeholder="Description de l'activité..." readonly><?php echo $activity["act_description"]; ?></textarea>
             </div>
         </div>
         <div class="field">
             <label for="act_resume">Description courte</label>
             <div class="control">
-                <textarea class="textarea" name="act_resume" placeholder="Description courte de l'activité..." readonly><?php echo $activity[0]["act_resume"]; ?></textarea>
+                <textarea class="textarea" name="act_resume" placeholder="Description courte de l'activité..." readonly><?php echo $activity["act_resume"]; ?></textarea>
             </div>
         </div>
         <div class="field">
             <label for="act_base_price">Prix hors promotion (€)</label>
             <div class="control">
-                <input class="input" value="<?php echo $activity[0]["act_base_price"]; ?>" min="0.00" step="0.01" name="act_base_price" readonly>
+                <input class="input" value="<?php echo $activity["act_base_price"]; ?>" min="0.00" step="0.01" name="act_base_price" readonly>
             </div>
         </div>
         <div class="field">
             <label for="act_duration">Durée d'une session (min)</label>
             <div class="control">
-                <input class="input" value="<?php echo $activity[0]["act_duration"]; ?>" name="act_duration" readonly>
+                <input class="input" value="<?php echo $activity["act_duration"]; ?>" name="act_duration" readonly>
             </div>
         </div>
         <div class="field">
             <label for="act_monitor_nb">Nombre de moniteur requis</label>
             <div class="control">
-                <input class="input" value="<?php echo $activity[0]["act_monitor_nb"]; ?>" min="0" name="act_monitor_nb" readonly>
+                <input class="input" value="<?php echo $activity["act_monitor_nb"]; ?>" min="0" name="act_monitor_nb" readonly>
             </div>
         </div>
         <div class="field">
             <label for="act_operator_nb">Nombre d'opérateur requis</label>
             <div class="control">
-                <input class="input" type="number" value="<?php echo $activity[0]["act_operator_nb"]; ?>" min="0" name="act_operator_nb" readonly>
+                <input class="input" type="number" value="<?php echo $activity["act_operator_nb"]; ?>" min="0" name="act_operator_nb" readonly>
             </div>
         </div>
         <div class="field">
@@ -70,11 +70,11 @@
         <form method="post" action="scheduleActivity">
         <div class="field">
         Début
-        <input type="date" name="date_start">
+        <input type="date" name="date_start" required>
         Fin
-        <input type="date" name="date_end">
+        <input type="date" name="date_end" required>
         </div>
-        <div class="field">
+        <div class="field" >
         Jours :
             <label class="checkbox">
                 Lundi
@@ -107,12 +107,12 @@
         </div>
         <div class="field">
             Heure Début
-            <input type="time" name="hour_start">
+            <input type="time" name="hour_start" required>
             Heure Fin
-            <input type="time" name="hour_end">
+            <input type="time" name="hour_end" required>
         </div>
             <div class="field">
-                <input type="hidden" name="act_id" value="<?php echo $activity[0]["act_id"]; ?>">
+                <input type="hidden" name="act_id" value="<?php echo $activity["act_id"]; ?>">
                 <div class="control">
                     <button class="button is-link">Valider</button>
                 </div>
@@ -120,5 +120,24 @@
     </div>
     <?php endif; ?>
 </div>
-
-
+<script>
+    // // Initialize all input of date type.
+    // const calendars = bulmaCalendar.attach('[type="date"]', '[displayMode="inline"]');
+    //
+    // // Loop on each calendar initialized
+    // calendars.forEach(calendar => {
+    //     // Add listener to date:selected event
+    //     calendar.on('date:selected', date => {
+    //         console.log(date);
+    //     });
+    // });
+    //
+    // // To access to bulmaCalendar instance of an element
+    // const element = document.querySelector('#my-element');
+    // if (element) {
+    //     // bulmaCalendar instance is available as element.bulmaCalendar
+    //     element.bulmaCalendar.on('select', datepicker => {
+    //         console.log(datepicker.data.value());
+    //     });
+    // }
+</script>

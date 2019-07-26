1ede58e1e1c196853b27c233229ae050c2d2d238
Corentin FRANTZ <frantzcorentin@gmail.com>
2019-07-26 09:51:41 +0200

    Calendar wip

--- application/views/planningActivity.php
+++ application/views/planningActivity.php
@@ -8,7 +8,36 @@
         var calendarEl = document.getElementById('calendar');
 
         var calendar = new FullCalendar.Calendar(calendarEl, {
-            plugins: [ 'dayGrid', 'timeGrid', 'list' ] // an array of strings!
+            plugins: [ 'dayGrid', 'timeGrid', 'list' ], // an array of strings!
+            locale: 'fr',
+            defaultView: 'timeGridWeek',
+            eventSources: [
+
+                // your event source
+                {
+                    events: [ // put the array in the `events` property
+                        {
+                            title  : 'event1',
+                            start  : '2019-07-01T00:00:00-05:00',
+                            end: '2019-07-12T00:00:00-05:00'
+                        },
+                        {
+                            title  : 'event2',
+                            start  : '2019-07-26',
+                            end    : '2019-07-28'
+                        },
+                        {
+                            title  : 'event3',
+                            start  : '2019-07-29T12:30:00',
+                        }
+                    ],
+                    color: 'white',     // an option!
+                    textColor: 'blue' // an option!
+                }
+
+                // any other event sources...
+
+            ]
         });
 
         calendar.render();

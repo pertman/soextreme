f16d0b1b148d53b983777f8cff9331614b83fc4e
Pertman <Pertman>
2019-09-10 19:30:08 +0200

    Age / Email gift / Style

--- application/views/reservationForm.php
+++ application/views/reservationForm.php
@@ -22,18 +22,21 @@
     </div>
 </div>
 
-<div class="field">
-    <div class="control">
+
+    <div class="buttons">
         <button class="button is-link validate-participant-nb">Valider</button>
     </div>
-</div>
+
 
 <form class="reservationForm" method="post" action="create">
-    <div class="participants"></div>
+    <div class="participants">
+    </div>
     <input type="hidden" name="tsl_id" value="<?php echo $tslId; ?>">
-    <div class="field">
-        <div class="control">
-            <button class="button is-link">Valider</button>
+    <div class="buttons">
+        <div class="field">
+            <div class="control">
+                <button class="button validate-reservation-form hidden is-link">Valider</button>
+            </div>
         </div>
     </div>
 </form>
@@ -44,6 +47,8 @@
         let participants = $('.participants');
 
         let currentParticipantsCount = $('.participant').length;
+
+        $('.validate-reservation-form').removeClass('hidden');
 
         if (currentParticipantsCount === participantNb){
             return true;
@@ -59,27 +64,45 @@
             for (let i = currentParticipantsCount; i < participantNb; i++){
                 let participantIndex =  parseInt(i) + 1;
 
-                participants.append('<div class="participant participant-' + participantIndex + '">\n' +
-                    '            <h1>Participant n°' + participantIndex + '</h1>\n' +
-                    '            <div class="columns">\n' +
-                    '                <div class="column">\n' +
-                    '                    <div class="field">\n' +
-                    '                        <label for="usr_firstname">Prénom</label>\n' +
-                    '                        <div class="control">\n' +
-                    '                            <input type="text" class="input" name="participant-[' + participantIndex + '][usr_firstname]" required>\n' +
-                    '                        </div>\n' +
-                    '                    </div>\n' +
-                    '                </div>\n' +
-                    '                <div class="column">\n' +
-                    '                    <div class="field">\n' +
-                    '                        <label for="usr_lastname">Nom</label>\n' +
-                    '                        <div class="control">\n' +
-                    '                            <input type="text" class="input" name="participant-[' + participantIndex + '][usr_lastname]" required>\n' +
-                    '                        </div>\n' +
-                    '                    </div>\n' +
-                    '                </div>\n' +
-                    '            </div>\n' +
-                    '        </div>')
+                participants.append('<div class="participant participant-' + participantIndex + '">\n'+
+'            <div class="participant-label">Participant n°' + participantIndex + '</div>\n'+
+'            <div class="columns">\n'+
+'                <div class="column">\n'+
+'                    <div class="field">\n'+
+'                        <label for="usr_firstname">Prénom</label>\n'+
+'                        <div class="control">\n'+
+'                            <input type="text" class="input" name="participant-[' + participantIndex + '][usr_firstname]" required>\n'+
+'                        </div>\n'+
+'                    </div>\n'+
+'                </div>\n'+
+'                <div class="column">\n'+
+'                    <div class="field">\n'+
+'                        <label for="usr_lastname">Nom</label>\n'+
+'                        <div class="control">\n'+
+'                            <input type="text" class="input" name="participant-[' + participantIndex + '][usr_lastname]" required>\n'+
+'                        </div>\n'+
+'                     </div>\n'+
+'                 </div>\n'+
+'            </div>\n'+
+'            <div class="columns">\n'+
+'                <div class="column">\n'+
+'                    <div class="field">\n'+
+'                        <label for="usr_age">Age</label>\n'+
+'                        <div class="control">\n'+
+'                            <input type="number" class="input" min="<?php echo $activity['act_required_age']; ?>" name="participant-[' + participantIndex + '][usr_age]" required>\n'+
+'                        </div>\n'+
+'                    </div>\n'+
+'                </div>\n'+
+'                <div class="column">\n'+
+'                    <div class="field">\n'+
+'                       <label for="usr_gift_email">Email du destinataire du cadeau (Optionnel) </label>\n'+
+'                         <div class="control">\n'+
+'                           <input type="text" class="input" name="participant-[' + participantIndex + '][usr_gift_email]">\n'+
+'                        </div>\n'+
+'                    </div>\n'+
+'                </div>\n'+
+'            </div>\n'+
+'        </div>')
             }
         }
     })

5b1545e93aa114373ecbd44e88493f81db340bcd
Corentin FRANTZ <frantzcorentin@gmail.com>
2019-09-12 16:02:27 +0200

    Ajout header pour authoriser les appels IONIC

--- application/controllers/ApiController.php
+++ application/controllers/ApiController.php
@@ -9,22 +9,19 @@ class ApiController extends MY_Controller {
     {
         parent::__construct();
         parent::init();
-
+        header('Access-Control-Allow-Origin: *');
+        header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method, Authorization");
+        header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
         //@TODO check security
     }
 
     public function getActivities(){
-        header('Access-Control-Allow-Origin: *');
-        header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method, Authorization");
-        header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+
         $activities = $this->ActivityModel->getAllActivities();
         die(json_encode($activities));
     }
 
     public function getActivityById(){
-        header('Access-Control-Allow-Origin: *');
-        header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method, Authorization");
-        header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
         $actId = $this->input->get('id');
         if (!$actId){
             die('Error id parameter missing');
